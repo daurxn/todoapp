2963790bfbf38ff2c7b0753e00b767750140fc64
dauren.e <d.esmuhanov@qazcloud.kz>
2025-06-13 18:00:51 +0500

    values

--- requests.py
+++ requests.py
@@ -54,5 +54,5 @@ async def add_task(user_id, title):
         
 async def update_task(task_id):
     async with async_session() as session:
-        await session.execute(update(Task).where(Task.id == task_id).value(completed=True))
+        await session.execute(update(Task).where(Task.id == task_id).values(completed=True))
         await session.commit()
